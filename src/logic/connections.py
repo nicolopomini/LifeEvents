8c9b2262d065676ed0e567db90cfbae02b5392d2
Nicolò Pomini <nicolo.pomini@u-hopper.com>
2018-10-30 09:41:58 +0100

    issue with importing a file relative path fixed

--- src/logic/connections.py
+++ src/logic/connections.py
@@ -4,15 +4,16 @@ from __future__ import annotations
 import json
 
 from tapoi.tapoi import Tapoi
+import os
 
+TEST_FILENAME = os.path.join(os.path.dirname(__file__), 'test.txt')
 
 class TapoiConnection:
 
     @staticmethod
     def get_connection() -> Tapoi:
+        KEYS_FILE = os.path.join(os.path.dirname(__file__), 'keys.json')
         keys = None
-        with open('keys.json') as f:
+        with open(KEYS_FILE) as f:
             keys = json.load(f)
         return Tapoi(keys["key"], keys["secret"])
-
-TapoiConnection.get_connection()
