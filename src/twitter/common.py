dd48c8dd8bc22761ea9a04e684c1851cd82761c2
Nicolò Pomini <nicolo.pomini@u-hopper.com>
2018-11-22 17:59:00 +0100

    Removed useless files and deleted an useless print

--- src/twitter/common.py
+++
@@ -1,8 +0,0 @@
-from __future__ import absolute_import
-from __future__ import annotations
-
-class Keys:
-    CONSUMER_KEY = "olGFsP7jtEU8zkcbouqfhA68N"
-    CONSUMER_SECRET = "V4Q7d56Q0Tn8BYshJCC1DfLZAy80rOGsKAha5KhJeuIcoRSFtq"
-    ACCESS_KEY = "221366410-04wtYwNV5WthbResxWMxaLAqjqkYZrDVkHM9eJCe"
-    ACCESS_SECRET = "78GEUH9xRnpQ2av7B1zIIov2mBqFfPO5qWr7K90Oy7H6q"
